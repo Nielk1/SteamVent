40a93aad89109bdd6b834e5e2ffc35d2527464d8
Nielk1 <development@nielk1.com>
2020-09-26 20:14:30 -0500

    added steamcmd tests

---
+++ SteamVent.Tests/SteamCmd/SteamCmdTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using SteamVent.SteamCmd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamVent.Tests.SteamCmd
+{
+    [TestFixture]
+    public class SteamCmdTests
+    {
+        private SteamCmdContext steamcmd;
+        [SetUp]
+        public void BaseSetUp()
+        {
+            steamcmd = SteamCmdContext.GetInstance();
+            Assert.IsNotNull(steamcmd);
+            steamcmd.Download();
+        }
+
+        [Test, Order(1)]
+        public void WorkshopStatusTest()
+        {
+            List<WorkshopItemStatus> status = steamcmd.WorkshopStatus(624970);
+
+            StringBuilder bld = new StringBuilder();
+            bld.AppendLine("WorkshopId\tStatus   \tHasUpdate\tSize\tDateTime");
+            foreach (var stat in status)
+            {
+                bld.AppendLine($"{stat.WorkshopId}\t{stat.Status}\t{stat.HasUpdate}    \t{stat.Size}\t{stat.DateTime}");
+            }
+            Assert.Pass(bld.ToString());
+        }
+
+        [Test, Order(2)]
+        public void WorkshopDownloadItemTest()
+        {
+            string downloadString = steamcmd.WorkshopDownloadItem(624970, 1762479746);
+
+            List<WorkshopItemStatus> status = steamcmd.WorkshopStatus(624970);
+            Assert.Greater(status.Count, 0);
+            Assert.IsTrue(status.Any(stat => stat.WorkshopId == 1762479746 && stat.Status == "installed"));
+
+            Assert.Pass(downloadString);
+        }
+    }
+}
